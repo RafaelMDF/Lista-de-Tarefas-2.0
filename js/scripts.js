034a5a2c0ee392303595fa55bc8e52f53a390579
Rafael Motta de Freitas <105321718+RafaelMDF@users.noreply.github.com>
2024-06-12 23:36:09 -0300

    Atualização e correção  da ativação do botão

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,25 @@ let inputNovaTarefa = document.querySelector("#inputNovaTarefa");
 let btnAddTarefa = document.querySelector("#btnAddTarefa");
 let listaTarefas = document.querySelector("#listaTarefas");
 
-inputNovaTarefa.addventListener("keypress", (e) => {
-  if (e.keyCode == 13) {
+inputNovaTarefa.addEventListener("keypress", (e) => {
+  if (e.keyCode === 13) {
     let tarefa = {
       nome: inputNovaTarefa.value,
-      id: gerarid(),
+      id: gerarId(),
     }
     adicionarTarefa(tarefa);
   }
 });
 
-btnAddTarefa.addEventListener("click", (e) => {
+btnAddTarefa.addEventListener("click", () => {
   let tarefa = {
     nome: inputNovaTarefa.value,
-    id: gerarid(),
+    id: gerarId(),
   }
   adicionarTarefa(tarefa);
 });
 
-function gerarid() {
+function gerarId() {
   return Math.floor(Math.random() * 3000);
 }
 
@@ -31,22 +31,23 @@ function adicionarTarefa(tarefa) {
 }
 
 function criarTagLI(tarefa) {
-
   let li = document.createElement("li");
 
   let span = document.createElement("span");
   span.classList.add("textoTarefa");
-  span.innerHTML = tarefa.none;
+  span.innerHTML = tarefa.nome;
 
   let div = document.createElement("div");
 
   let btnEditar = document.createElement("button");
   btnEditar.classList.add("btnAcao");
   btnEditar.innerHTML = '<i class="fa fa-pencil"></i>';
+  btnEditar.addEventListener("click", () => editarTarefa(tarefa.id));
 
   let btnExcluir = document.createElement("button");
   btnExcluir.classList.add("btnAcao");
   btnExcluir.innerHTML = '<i class="fa fa-trash"></i>';
+  btnExcluir.addEventListener("click", () => excluirTarefa(tarefa.id, li));
 
   div.appendChild(btnEditar);
   div.appendChild(btnExcluir);
@@ -54,4 +55,15 @@ function criarTagLI(tarefa) {
   li.appendChild(span);
   li.appendChild(div);
   return li;
+}
+
+function editarTarefa(id) {
+  // Lógica para editar a tarefa
+  console.log("Editar tarefa com ID:", id);
+}
+
+function excluirTarefa(id, li) {
+  // Lógica para excluir a tarefa
+  li.remove();
+  console.log("Excluir tarefa com ID:", id);
 }
